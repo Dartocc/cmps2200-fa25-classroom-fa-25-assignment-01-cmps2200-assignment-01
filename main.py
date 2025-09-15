5eab1bef993ef2df3e665ed30f554273c9329fd9
Dartocc <dtocci@tulane.edu>
2025-09-15 13:22:51 -0500

    Finished

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ def foo(x):
       return (foo(x - 1) + foo(x - 2))
 
 def longest_run(mylist, key):
-  run = 0
-  max_run = 0
-  for x in mylist:
-    if x == key:
-      run += 1
-    else:
-      if run > max_run:
-        max_run = run
-        run = 0
-      else:
-        run = 0
-  return max_run
+    counter = 0
+    maxcount = 0
+    for i in mylist:
+        if i == key:
+            counter += 1
+            maxcount = max(maxcount, counter)
+        else:
+            counter = 0
+    return maxcount
+
+
 
 class Result:
+    """ done """
     def __init__(self, left_size, right_size, longest_size, is_entire_range):
         self.left_size = left_size               # run on left side of input
         self.right_size = right_size             # run on right side of input
@@ -34,46 +34,48 @@ class Result:
     def __repr__(self):
         return('longest_size=%d left_size=%d right_size=%d is_entire_range=%s' %
               (self.longest_size, self.left_size, self.right_size, self.is_entire_range))
-  
+    
+
+def to_value(v):
+    """
+    if it is a Result object, return longest_size.
+    else return v
+    """
+    if type(v) == Result:
+        return v.longest_size
+    else:
+        return int(v)
+        
 def longest_run_recursive(mylist, key):
-  if len(mylist) == 1:
-    if mylist[0] == key:
-      return Result(1, 1, 1, True)
-    else:
-      return Result(0, 0, 0, False)
-  elif len(mylist) == 0:
-    return Result(0, 0, 0, False)
-  else:
+    # Base cases: If the list is empty or if it only has one item in it, check if the item is what we need
+    if len(mylist) == 0:
+        return Result(0, 0, 0, False)
+    elif len(mylist) == 1:
+        if mylist[0] == key:
+            return Result(1, 1, 1, True)
+        else:
+            return Result(0, 0, 0, False)
+    
     mid = len(mylist) // 2
-    l = mylist[:mid]
-    r = mylist[mid:]
-    a0 = Result(0, 0, 0, False)
-    l0 = longest_run_recursive(l, key)
-    r0 = longest_run_recursive(r, key)
-
-    if l0.is_entire_range == True and r0.is_entire_range == True:
-      a0.left_size = l0.longest_size + r0.longest_size
-      a0.longest_size = l0.longest_size + r0.longest_size
-      a0.right_size = l0.longest_size + r0.longest_size
-      a0.is_entire_range = True
-      return a0
-    else:
-      if l0.is_entire_range == True:
-        a0.left_size = l0.longest_size + r0.left_size
-        a0.longest_size = max(l0.longest_size, r0.longest_size, l0.right_size + r0.left_size)
-        a0.right_size = r0.right_size
-      elif r0.is_entire_range == True:
-        a0.right_size = r0.longest_size + l0.right_size
-        a0.left_size = l0.left_size
-        a0.longest_size = max(l0.longest_size, r0.longest_size, l0.right_size + r0.left_size)
-      else:
-        a0.left_size = l0.left_size
-        a0.longest_size = max(l0.longest_size, r0.longest_size, l0.right_size + r0.left_size)
-        a0.right_size = r0.right_size
-      return a0
-
-def test_longest_run():
-    assert longest_run([2,12,12,8,12,12,12,0,12,1], 12) == 3
-
-def test_longest_run_recursive():
-    assert longest_run_recursive([2,12,12,8,12,12,12,0,12,1], 12).longest_size == 3
+    left_result = longest_run_recursive(mylist[:mid], key)
+    right_result = longest_run_recursive(mylist[mid:], key)
+    
+    # Determine if entire range matches key
+    is_entire = left_result.is_entire_range and right_result.is_entire_range
+    
+    # Left size
+    left_size = left_result.left_size
+    if left_result.is_entire_range:
+        left_size += right_result.left_size
+    
+    # Right size
+    right_size = right_result.right_size
+    if right_result.is_entire_range:
+        right_size += left_result.right_size
+    
+    # Longest size across middle
+    longest_size = max(left_result.longest_size, 
+                       right_result.longest_size,
+                       left_result.right_size + right_result.left_size)
+    
+    return Result(left_size, right_size, longest_size, is_entire)
